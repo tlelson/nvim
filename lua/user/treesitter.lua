c476438e649ce16901624ed5cd6c199e30a44992
tlelson <tpj800@gmail.com>
2022-10-19 10:42:12 +1100

    add markdown to treesitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -4,7 +4,8 @@ if not status_ok then
 end
 
 configs.setup({
-	ensure_installed = "lua", "python", "json", "go", "bash", "make", "dockerfile", "yaml",
+	-- test new with `:TSInstall markdown`
+	ensure_installed = "lua", "python", "json", "go", "bash", "make", "dockerfile", "yaml", "markdown",
 	sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
 	ignore_install = { "" }, -- List of parsers to ignore installing
 	highlight = {
