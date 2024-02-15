4a29f05142b4d32798ff26ad62d5c8dc648aa8fa
tlelson <tpj800@gmail.com>
2024-02-15 13:26:59 +1100

    Add autocmd to remove trailing whitespace

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -13,23 +13,9 @@ vim.api.nvim_create_autocmd("TextYankPost", {
   pattern = "*",
 })
 
--- Hack until I can work out why these are added
--- Fix Puts that end up with \r\n after them for who-knows-what reason
---local group = vim.api.nvim_create_augroup("dos-line-ending-remover", { clear = true })
---vim.api.nvim_create_autocmd("BufWritePre", { command = "%s/\r//g", group = group })
---vim.api.nvim_create_autocmd("BufWritePre", { command = ":%g/^\r\n/d | :%s/\r//g", group = group })
---
---local line_ending_grp = vim.api.nvim_create_augroup("LineEndingGroup", opts?)
---vim.api.nvim_create_autocmd("BufReadPost", {
---callback = function()
---vim.cmd([[
---if !exists('b:reload_dos') && !&binary && &ff=='unix' && (0 < search('\r$', 'nc')) |
---\   let b:reload_dos = 1 |
---\   e ++ff=dos |
---\ endif
---]])
---end,
-----group = highlight_group,
---nested = true,
---pattern = "*",
---})
+local remove_whitespace = vim.api.nvim_create_augroup("RemoveWhitespace", { clear = true })
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = remove_whitespace,
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
