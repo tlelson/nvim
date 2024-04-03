2c7285fc5270d16e9314ff002dc66f46f46dfd6f
tlelson <tpj800@gmail.com>
2024-04-03 11:01:15 +1100

    Enable Go LSP and Disable a Keymap
    <C-k> is used to write diagraphs.  I don't need it in insert mode.

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
+        "gopls",
         "stylua",
         "shellcheck",
         "shfmt",
@@ -18,6 +19,16 @@ return {
   },
   {
     "neovim/nvim-lspconfig",
+    init = function()
+      -- Cusomise the keymaps
+      local keys = require("lazyvim.plugins.lsp.keymaps").get()
+      -- change a keymap
+      --keys[#keys + 1] = { "K", "<cmd>echo 'hello'<cr>" }
+      -- disable a keymap
+      keys[#keys + 1] = { "<C-k>", mode = { "i" }, false }
+      -- add a keymap
+      --keys[#keys + 1] = { "H", "<cmd>echo 'hello'<cr>" }
+    end,
     ---@class PluginLspOpts
     opts = {
       ---@type lspconfig.options
