b2f852e50a65f2dfc795aa699366f9b4853b3139
Tim <tpj800@gmail.com>
2024-11-22 12:35:57 +1100

    Update Lazy and Neovim

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,5 +1,15 @@
 -- LazyVim Core Plugin config
 return {
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
   { "mini.nvim", enabled = false },
   { "flash.nvim", enabled = false },
   {
